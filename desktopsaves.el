8c86fc3d0e80359bf5819b9d4237b096a563427a
Nils Creque & Steve Shogren <sashogre@us.ibm.com>
2014-12-03 10:29:50 -0500

    Updating cider and company mode, reenabling auto-revert-mode

--- desktopsaves.el
+++ desktopsaves.el
@@ -1,5 +1,5 @@
 ;; makes emacs reload buffers without asking
-;; (global-auto-revert-mode t)
+(global-auto-revert-mode t)
 
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
