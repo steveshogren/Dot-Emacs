88b2b9879d5af08e0f4d85680360683bef6be86a
Steve Shogren <steve.a.shogren@gmail.com>
2014-12-03 16:00:00 -0500

    Adding repl shortcuts for racket

--- scheme-settings.el
+++ scheme-settings.el
@@ -2,3 +2,7 @@
 (add-hook 'scheme-mode-hook 'paredit-mode)
 (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scheme-mode-hook 'auto-complete-mode)
+
+(define-key scheme-mode-map (kbd "C-M-x")   'lisp-eval-defun)
+(define-key scheme-mode-map (kbd "C-x C-e") 'lisp-eval-last-sexp)
+(define-key scheme-mode-map (kbd "C-c C-e") 'lisp-eval-last-sexp)
